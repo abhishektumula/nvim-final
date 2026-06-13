e33dddf5fab0341da8c7179820daa1852a2418fd
abhishektumula <abhishektumula@gmail.com>
2026-06-13 18:08:08 +0530

    refined

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,29 +1,18 @@
 return {
 	{
-		"L3MON4D3/LuaSnip",
-		dependencies = { "saadparwaiz1/cmp_luasnip", "rafamadriz/friendly-snippets" },
-	},
-	{
 		"hrsh7th/nvim-cmp",
 		dependencies = {
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
-			"L3MON4D3/LuaSnip",
-			"saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
 			local cmp = require("cmp")
-			local luasnip = require("luasnip")
-			require("luasnip.loaders.from_vscode").lazy_load()
+
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			vim.lsp.config("*", { capabilities = capabilities })
 
 			cmp.setup({
-				snippet = {
-					expand = function(args)
-						luasnip.lsp_expand(args.body)
-					end,
-				},
 				window = {
 					completion = cmp.config.window.bordered(),
 					documentation = cmp.config.window.bordered(),
@@ -37,8 +26,6 @@ return {
 					["<Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
-						elseif luasnip.expand_or_jumpable() then
-							luasnip.expand_or_jump()
 						else
 							fallback()
 						end
@@ -46,8 +33,6 @@ return {
 					["<S-Tab>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
 						else
 							fallback()
 						end
@@ -55,7 +40,6 @@ return {
 				}),
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
 					{ name = "path" },
 				}, {
 					{ name = "buffer" },
--- lua/vim-defaults.lua
+++ lua/vim-defaults.lua
@@ -2,18 +2,18 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.g.mapleader = " "
 vim.opt.numberwidth = 6
-vim.opt.tabstop = 2      -- Visual width of a tab
-vim.opt.shiftwidth = 2   -- Width of an indent
-vim.opt.expandtab = true  -- Convert tabs to spaces
--- color set to black 
+vim.opt.tabstop = 2 -- Visual width of a tab
+vim.opt.shiftwidth = 2 -- Width of an indent
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.clipboard = "unnamedplus"
+-- color set to black
 
 vim.api.nvim_create_autocmd("ColorScheme", {
-    callback = function()
-        vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "LineNr", { bg = "#000000" })
-        vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
-    end,
+	callback = function()
+		vim.api.nvim_set_hl(0, "Normal", { bg = "#000000" })
+		vim.api.nvim_set_hl(0, "NormalNC", { bg = "#000000" })
+		vim.api.nvim_set_hl(0, "SignColumn", { bg = "#000000" })
+		vim.api.nvim_set_hl(0, "LineNr", { bg = "#000000" })
+		vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "#000000" })
+	end,
 })
-
